3e7cd41a9dd65d62779fbe7f4aed983af34966fa
Git-my-stap <drumgog1@gmail.com>
2024-07-27 16:36:54 +0300

    Проftest

---
+++ proba.py
@@ -0,0 +1,13 @@
+stroka = input()# ввод
+powtor = 1# счетчик повторов
+symbol = ''# то, что повторяется к примеру а=symbol powtor=3 --> a3
+kolich = len(stroka)# считаем количество символов в строке
+for i in range(1 , kolich):# перебираем символы в строке
+    if stroka [i - 1] == stroka [i] :# если текущий символ такой же, как следующий, то
+        powtor = powtor + 1# к счетчику повторов добавляем 1
+    else:# иначе
+        symbol = symbol + (stroka[i - 1] + str(powtor))# к символу из строки добавляем строчную цифру из счетчика повторов чтоб получилось вида а5
+        powtor = 1# обнуляем
+if kolich:  #
+    symbol = symbol + (stroka[-1] + str(powtor))
+print(symbol)#
